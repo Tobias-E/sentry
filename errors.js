d6af610b9db048ec8129848cb90b983c3e349884
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-02-11 13:14:54 +0100

    reference errors

--- errors.js
+++ errors.js
@@ -1,7 +1,29 @@
 // Example 1
-myUndefinedFunction();
-hello();
+// ReferenceError: myUndefinedFunction is not defined
+// myUndefinedFunction();
 
 // Example 2
-document.querySelector('hello') = hello;
-console.log(hello)
+// ReferenceError: cannot assign to function call
+// document.querySelector('hello') = hello;
+
+// Example 3
+// ReferenceError: hello is not defined
+// hello
+
+// Example 4
+// ReferenceError: arr is not defined
+// arr[0,2]
+
+// Example 5
+// ReferenceError: inner is not defined
+/* function container() {
+    const inner = 4
+}
+console.log(inner) */
+
+// Example 6 
+// ReferenceError: hell is not defined
+/* const hello = 4;
+hell */
+
+// Example 7
