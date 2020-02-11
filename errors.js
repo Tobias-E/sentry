e0733b591790f9a3637d6df79f5ccca5f61797bc
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-02-11 12:25:57 +0100

    2

--- errors.js
+++ errors.js
@@ -2,4 +2,6 @@
 myUndefinedFunction();
 
 // Example 2
-console.log(test)
+let params = new URLSearchParams(document.location.search);
+params.get('destination_id')
+console.log(params.get('destination_id'))
